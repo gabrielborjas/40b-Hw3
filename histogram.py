1ad774ec63aabcc7c8b934c58e6dd520502a2154
gabrielborjas <gborjasperez@ucsd.edu>
2025-10-22 16:03:58 -0700

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -1,1 +1,21 @@
-print('Hello World')
+def histogram(points, bins):
+	n = len(points)
+	k = len(bins)
+	all_densities = []
+	i = 0
+
+	for x,y in bins:
+		count = 0
+		while i < n and points[i]:
+			i += 1
+
+		start_index = i
+		while i < n and points[i] < b:
+			count += 1
+		count = i - start_index
+
+		width = b-a
+		density= count/(n* width)
+		all_densities.append(density)
+
+	return all_densities
