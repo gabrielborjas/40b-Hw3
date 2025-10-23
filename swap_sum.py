19da44c849fe11b1d6f7a3cab021c601d3522602
gabrielborjas <gborjasperez@ucsd.edu>
2025-10-22 23:55:20 -0700

    Update swap_sum.py

--- swap_sum.py
+++ swap_sum.py
@@ -1,1 +1,27 @@
+def swap_sum(A, B):
+	sum_A = sum(A)
+	sum_B = sum(B)
+	difference = sum_A - sum_B
 
+	if difference % 2 != 0:
+		return None
+
+	target = difference // 2 
+
+	i = 0
+	j = 0
+
+	len_A = len(A)
+	len_B = len(B)
+
+	while i < len_A and j < len_B:
+		val = A[i] - B[j]
+
+		if val == target:
+			return (i, j)
+		elif val < target:
+			i += 1
+		else:
+			j += 1
+
+	return None
